ceb7740929ecb7031bceb811dda5cb9c2c29552a
Gosplex <johngospel003@gmail.com>
2025-05-20 06:54:50 +0530

    completed fundme functionality demo

--- contracts/Solidity_basics_fund_me/FundMe.sol
+++ contracts/Solidity_basics_fund_me/FundMe.sol
@@ -12,6 +12,12 @@ contract FundMe {
     address[] public funders;
     mapping (address => uint256) public fundersAddress;
 
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
     function fund() public payable {
         require(
             msg.value.getConversionRate() >= MINIMUM_AMOUNT,
@@ -23,5 +29,37 @@ contract FundMe {
         fundersAddress[payer] += msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
+        {
+           address funder = funders[funderIndex];
+           fundersAddress[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        // transfer the eth
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Failed to send Ether");
+
+        // call
+        (bool success,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Call failed");
+    }
+
+    modifier  onlyOwner() {
+        require(msg.sender == i_owner, "Not authorized");
+        _;
+    }
+
+
+    receive() external payable { 
+        fund();
+    }
+
+    fallback() external payable { 
+        fund();
+    }
 }
